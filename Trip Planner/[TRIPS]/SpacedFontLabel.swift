ca13126fdc3d9758af1e0b4a6d2c4dcee6768156
SlickJohnson <liwa.johnson@gmail.com>
2017-12-08 23:11:07 -0800

    Add login screen that appears if no user is logged in

---
+++ Trip Planner/[TRIPS]/SpacedFontLabel.swift
@@ -0,0 +1,47 @@
+import UIKit
+
+@IBDesignable class SpacedFontLabel: UILabel {
+
+  @IBInspectable var characterSpacing: CGFloat = 0.0 {
+    didSet {
+      adjustTextSpacing()
+    }
+  }
+
+  override var text: String? {
+    didSet {
+      adjustTextSpacing()
+    }
+  }
+
+  var attributedString: NSMutableAttributedString!
+
+  required init(coder aDecoder: NSCoder) {
+    super.init(coder: aDecoder)!
+    self.commonInit()
+  }
+
+  override init(frame: CGRect) {
+    super.init(frame: frame)
+    self.commonInit()
+  }
+
+  func commonInit() {
+    adjustTextSpacing()
+  }
+
+  func adjustTextSpacing() {
+    guard let text = text else { return }
+
+    if let attributedText = attributedText {
+      attributedString = NSMutableAttributedString(attributedString: attributedText)
+    } else {
+      attributedString = NSMutableAttributedString(string: text)
+    }
+
+    attributedString.addAttribute(.kern, value: characterSpacing,
+                                  range: NSRange(location: 0, length: attributedString.length))
+
+    attributedText = attributedString
+  }
+}
